041f28e7db83d99c57ba8d55347e93b03d47ce21
MakerYang <open@wileho.com>
2024-09-04 16:23:50 +0800

    update mic

--- example/example.py
+++ example/example.py
@@ -14,7 +14,7 @@ def on_start(sdk):
         sdk.Package.detection.start_stream(9)
         sdk.hardware.microphone.drive.control_listen()
     while not sdk.quit_event.is_set():
-        if sdk.Package.detection.recorder and len(sdk.Package.keyword.list) > 0:
+        if sdk.Package.detection.stream and len(sdk.Package.keyword.list) > 0:
             pcm = sdk.Package.detection.stream.read()
             result = sdk.Package.detection.porcupine.process(pcm)
             print(result)
